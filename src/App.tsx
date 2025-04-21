e5cfb42aa332c01148263bcc1731203f26bb620e
Ayushi <ayushisaxena24111999@gmail.com>
2025-04-21 12:54:07 +0530

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-// import Main from "./pages/Main";
-import ErrorPage from "./pages/ErrorPage";
-import Layout from "./components/Layout/Layout";
-import { store } from "./redux/store";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import React, { Suspense } from "react";
+import Loader from "./components/Loader";
+
+// Lazy-loaded components
+const Home = React.lazy(() => import("./components/Home/Home"));
+const Login = React.lazy(() => import("./pages/Login"));
+const Signup = React.lazy(() => import("./pages/Signup"));
+const ErrorPage = React.lazy(() => import("./pages/ErrorPage"));
+const Layout = React.lazy(() => import("./components/Layout/Layout"));
 
 function App() {
   return (
     <BrowserRouter basename="inotes">
       <Provider store={store}>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/mainpage" element={<Layout />} />
-          <Route path="/errorpage" element={<ErrorPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
+        <div className="App">
+          <Suspense fallback={<div><Loader /></div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/mainpage" element={<Layout />} />
+              <Route path="/errorpage" element={<ErrorPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Suspense>
+        </div>
       </Provider>
     </BrowserRouter>
   );
