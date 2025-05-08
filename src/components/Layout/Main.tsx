e4b01c93325dc129f036f299cb8294a51836b109
Ayushi <ayushisaxena24111999@gmail.com>
2025-05-08 11:13:00 +0530

    added maath file

--- src/components/Layout/Main.tsx
+++ src/components/Layout/Main.tsx
@@ -53,7 +53,7 @@ export default function Main({
     title: "",
     description: "",
   });
-
+console.log(errors, errorsHelperText)
   const validateForm = () => {
     let titleHelperText = "";
     let descriptionHelperText = "";
